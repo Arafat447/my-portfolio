48f3efca6c78870cbeb04a36ae3af1dbdde58c8a
Arafat447 <tahmidhasan184@gmail.com>
2022-03-08 00:01:00 +0600

    home page almost done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,27 +1,40 @@
-import React from 'react';
-import './Home.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./Home.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 const Home = () => {
-    return (
-        <div className=''>
-            <section>
-                <div className="container main">
-                    <div className=" d-flex">
-                        <div className="home-text">
-                            <p>Hello, I'm</p>
-                            <h1>Arafat Hossain</h1>
-                            <h2>Front End Web Developer</h2>
-                        </div>
-                        <div className="home-image">
-                            <div className="img-box">
-                                <img src="https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg" alt="" />
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div>
+      <section>
+        <div className="container ">
+          <div className="row align-items-center main mt-5">
+            <div className="col-md-6 home-text order-md-0 order-1">
+              <div className="ps-md-3">
+                <p>Hello, I'm</p>
+                <h1>Arafat Hossain</h1>
+                <h2>Front End Web Developer</h2>
+                <a href="#" className="btn btn-custom me-2">
+                  More About Me
+                </a>
+                <a href="#" className="btn btn-custom">
+                  Portfolio
+                </a>
+              </div>
+            </div>
+            <div className="col-md-6 order-md-1 order-0 order-lg-1">
+              <div className="home-image">
+                <div className="img-box text-center">
+                  <img
+                    src="https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"
+                    alt=""
+                  />
                 </div>
-            </section>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Home;
